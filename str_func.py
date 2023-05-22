67fb833c358215135160efd04243cc575336b4be
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-05-22 14:32:41 +0300

    add title_str func add docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def upper_str(value):
+    '''Возвращает строку заглавными буквами'''
     return value.upper()
+
+
+def title_str(value):
+    '''Возвращает строку с первыми заглавными буквами'''
+    return value.title()
